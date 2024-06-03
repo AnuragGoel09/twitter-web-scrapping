9af1568b378c0b0ff181bcf0d0acb8b22ae9859b
AnuragGoel09 <goelanurag2003@gmail.com>
2024-06-03 23:45:16 +0530

    chnage proxy

--- script.py
+++ script.py
@@ -21,17 +21,19 @@ proxymesh_url = "http://anurag09:anurag@us-wa.proxymesh.com:31280"
 
 def get_trending_topics():
     # Configure proxy
-    proxy = Proxy()
-    proxy.proxy_type = ProxyType.MANUAL
-    proxy.http_proxy = proxymesh_url
-    proxy.ssl_proxy = proxymesh_url
+    proxy = Proxy({
+        'proxyType': ProxyType.MANUAL,
+        'httpProxy': proxymesh_url,
+        'sslProxy': proxymesh_url,
+        'noProxy': ''  # set this value as desired
+    })
 
-    capabilities = webdriver.DesiredCapabilities.CHROME
-    proxy.add_to_capabilities(capabilities)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument(f'--proxy-server={proxymesh_url}')
 
     # Initialize the Chrome driver with proxy
     service = Service(chrome_driver_path)
-    driver = webdriver.Chrome(service=service, desired_capabilities=capabilities)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
     try:
         # Open Twitter login page
@@ -44,22 +46,21 @@ def get_trending_topics():
         password = driver.find_element(By.NAME, "password")
         password.send_keys("goelanurag2003")
         password.send_keys(Keys.RETURN)
-        WebDriverWait(driver,15).until(
+        WebDriverWait(driver, 15).until(
             EC.presence_of_element_located((By.XPATH, '//div[@aria-label="Timeline: Trending now"]'))
         )
-        
+
         # Locate the "What’s Happening" section and fetch the top 5 trending topics
-        trending_section=[]
-        for i in range(3,8):
+        trending_section = []
+        for i in range(3, 8):
             trending_section.append(driver.find_element(By.XPATH, f'//div[@aria-label="Timeline: Trending now"]/div/div[{i}]/div/div/div/div[2]/span'))
-       
 
         trends = [topic.text for topic in trending_section]
         end_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        
+
         # Fetch IP address used       
         driver.get("https://api.ipify.org?format=text")
-        ip_address = driver.find_element_by_xpath('html/body/pre').text
+        ip_address = driver.find_element(By.TAG_NAME, 'pre').text
 
         trend_data = {
             "trend1": trends[0],
@@ -70,9 +71,9 @@ def get_trending_topics():
             "end_time": end_time,
             "ip_address": ip_address
         }
-        
+
         collection.insert_one(trend_data)
-        
+
         def convert_objectid_to_string(obj):
             if isinstance(obj, ObjectId):
                 return str(obj)
@@ -81,8 +82,9 @@ def get_trending_topics():
             elif isinstance(obj, dict):
                 return {key: convert_objectid_to_string(value) for key, value in obj.items()}
             return obj
+
         return json.dumps(convert_objectid_to_string(trend_data))
-    
+
     finally:
         driver.quit()
 
